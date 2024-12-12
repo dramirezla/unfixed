b7e3ee0cddfcdda197ea39a3107c468b35e180a2
David Ramirez Lara <53583408+dramirezla@users.noreply.github.com>
2024-12-11 19:08:27 -0500

    Update AuthController.php
    cambio al authcontroller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,63 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function register(Request $request)
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
+         $request->validate([
+             'name' => 'required|string|max:255',
+             'email' => 'required|string|email|max:255|unique:users',
+             'password' => 'required|string|min:8|confirmed',
+         ]);
+         $user = User::create([
+             'name' => $request->name,
+             'email' => $request->email,
+             'password' => Hash::make($request->password),
+         ]);
+         return response()->json($user);
+     }
+    
+     public function login(Request $request)
+     {
+         $request->validate([
+             'email' => 'required|string|email',
+             'password' => 'required|string',
+     ]);
+     $user = User::where('email', $request->email)->first();
+     if (! $user || ! Hash::check($request->password, $user->password)) {
+         throw ValidationException::withMessages([
+             'email' => ['The provided credentials are incorrect.'],
+     ]);
+     }
+     $token = $user->createToken('token-name')->plainTextToken;
+     return response()->json(['token' => $token]);
+     }
 }
